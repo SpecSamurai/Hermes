685ece4f9d493e663ff69e0357b2c59b747c8a65
Spec <dawidjachowicz@proton.me>
2022-10-23 08:23:52 +0200

    Configure appsettings - Add appsettings.User.json configuration

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.User.json", optional: true, reloadOnChange: true);
+}
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
