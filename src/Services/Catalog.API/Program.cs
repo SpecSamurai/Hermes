1cd03946845271bf72db4001e46c2d4295d9fd07
Spec <dawidjachowicz@proton.me>
2022-10-24 09:14:36 +0200

    Add base configuration to handle Web API

--- src/Services/Catalog.API/Program.cs
+++ src/Services/Catalog.API/Program.cs
@@ -5,7 +5,20 @@ if (builder.Environment.IsDevelopment())
     builder.Configuration.AddJsonFile("appsettings.User.json", optional: true, reloadOnChange: true);
 }
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(configure =>
+{
+    configure.MapControllers();
+    configure.MapSwagger();
+});
 
 app.Run();
