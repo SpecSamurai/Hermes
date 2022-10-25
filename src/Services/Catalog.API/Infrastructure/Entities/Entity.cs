0033dd8274ba7923fb1c257e2c9975f65b60f3dc
Spec <dawidjachowicz@proton.me>
2022-10-25 07:30:28 +0200

    Add abstract Entity definition

---
+++ src/Services/Catalog.API/Infrastructure/Entities/Entity.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hermes.Catalog.API.Infrastructure.Entities;
+
+public abstract class Entity
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+    [MaxLength(255)]
+    public string CreatedBy { get; set; } = string.Empty;
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTime LastModifiedOn { get; set; }
+
+    [MaxLength(255)]
+    public string LastModifiedBy { get; set; } = string.Empty;
+
+    [Timestamp]
+    public byte[] RowVersion { get; set; } = { };
+}
