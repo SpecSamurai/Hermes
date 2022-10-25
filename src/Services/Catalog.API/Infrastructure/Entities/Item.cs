0ea38d8e2f52c8600e2888436ce73ab305c18954
Spec <dawidjachowicz@proton.me>
2022-10-25 07:31:26 +0200

    Add Item entity

---
+++ src/Services/Catalog.API/Infrastructure/Entities/Item.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hermes.Catalog.API.Infrastructure.Entities;
+
+public class Item : Entity
+{
+    [MaxLength(255)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(512)]
+    public string? Description { get; set; }
+
+    [Precision(10, 2)]
+    public decimal Price { get; set; }
+
+    [MaxLength(255)]
+    public string? PictureFileName { get; set; }
+
+    [MaxLength(255)]
+    public string? PictureUri { get; set; }
+    public int AvailableStock { get; set; }
+    public bool OnReorder { get; set; }
+
+    [ForeignKey(nameof(TypeId))]
+    public ItemType? Type { get; set; }
+    public Guid TypeId { get; set; }
+
+    [ForeignKey(nameof(BrandId))]
+    public Brand? Brand { get; set; }
+    public Guid BrandId { get; set; }
+}
