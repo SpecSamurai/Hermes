fc80f070cc2e731eaa1d0ee142bc574841a9c68b
Spec <dawidjachowicz@proton.me>
2022-11-03 22:10:50 +0100

    Add ISpecification

---
+++ src/Frameworks/Specifications/ISpecification.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace Hermes.Frameworks.Specifications;
+
+public interface ISpecification<T>
+{
+    bool IsSatisfiedBy(T @object);
+    Expression<Func<T, bool>> ToExpression();
+}
