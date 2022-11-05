0742cc41a78eb4c69aecaf1eefbbe5de839cd3ec
Spec <dawidjachowicz@proton.me>
2022-11-05 16:15:45 +0100

    Add ExpressionSpecification

---
+++ src/Frameworks/Specifications/ExpressionSpecification.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Hermes.Frameworks.Specifications;
+
+public class ExpressionSpecification<T> : Specification<T>
+{
+    private readonly Expression<Func<T, bool>> _expression;
+
+    public ExpressionSpecification(Expression<Func<T, bool>> expression) =>
+        _expression = expression;
+
+    public override Expression<Func<T, bool>> ToExpression() =>
+        _expression;
+}
