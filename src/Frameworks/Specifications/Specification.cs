1e2bbdc974e52bf3ae0cced0cb548f4d05a6ad8d
Spec <dawidjachowicz@proton.me>
2022-11-05 16:15:20 +0100

    Add abstract Specification

---
+++ src/Frameworks/Specifications/Specification.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Hermes.Frameworks.Specifications;
+
+public abstract class Specification<T> : ISpecification<T>
+{
+    public bool IsSatisfiedBy(T @object) =>
+        ToExpression().Compile()(@object);
+
+    public abstract Expression<Func<T, bool>> ToExpression();
+
+    public static implicit operator Expression<Func<T, bool>>(Specification<T> specification) =>
+        specification.ToExpression();
+}
