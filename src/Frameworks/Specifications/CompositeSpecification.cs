11aa040a698c6e9a0f23a211851a4a8659a59e05
Spec <dawidjachowicz@proton.me>
2022-11-05 16:15:55 +0100

    Add CompositeSpecification

---
+++ src/Frameworks/Specifications/CompositeSpecification.cs
@@ -0,0 +1,13 @@
+﻿namespace Hermes.Frameworks.Specifications;
+
+public abstract class CompositeSpecification<T> : Specification<T>, ICompositeSpecification<T>
+{
+    protected CompositeSpecification(ISpecification<T> left, ISpecification<T> right)
+    {
+        Left = left;
+        Right = right;
+    }
+
+    public ISpecification<T> Left { get; }
+    public ISpecification<T> Right { get; }
+}
