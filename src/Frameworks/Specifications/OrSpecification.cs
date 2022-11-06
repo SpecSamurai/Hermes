132898222f74694626e85b8b93127a47a32067a1
Spec <dawidjachowicz@proton.me>
2022-11-06 12:40:18 +0100

    Add OrSpecification

---
+++ src/Frameworks/Specifications/OrSpecification.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using LinqKit;
+
+namespace Hermes.Frameworks.Specifications;
+
+public class OrSpecification<T> : CompositeSpecification<T>
+{
+    public OrSpecification(ISpecification<T> left, ISpecification<T> right) : base(left, right)
+    {
+    }
+
+    public override Expression<Func<T, bool>> ToExpression() =>
+        Left.ToExpression().Or(Right.ToExpression());
+}
