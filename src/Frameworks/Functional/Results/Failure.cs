98fc1ae8078c204ee815bf1bf14a4f5f51f873d6
Spec <dawidjachowicz@proton.me>
2022-11-08 07:32:30 +0100

    Add an implementation of Failure

---
+++ src/Frameworks/Functional/Results/Failure.cs
@@ -0,0 +1,25 @@
+namespace Hermes.Frameworks.Functional.Results;
+
+public record Failure<TValue> : IResult<TValue>
+{
+    public Failure(ErrorMessage errorMessage) =>
+        ErrorMessage = errorMessage ?? throw new ArgumentNullException(nameof(errorMessage));
+
+    public ErrorMessage ErrorMessage { get; }
+
+    public TOutput Match<TOutput>(
+        Func<TValue, TOutput> onSuccess,
+        Func<ErrorMessage, TOutput> onFailure) =>
+            onFailure(ErrorMessage);
+
+    public Task<TOutput> MatchAsync<TOutput>(
+        Func<TValue, Task<TOutput>> onSuccess,
+        Func<ErrorMessage, TOutput> onFailure) =>
+            Task.FromResult(onFailure(ErrorMessage));
+
+    public static implicit operator ErrorMessage(Failure<TValue> failure) =>
+        failure.ErrorMessage;
+
+    public static implicit operator Failure<TValue>(ErrorMessage errorMessage) =>
+        new Failure<TValue>(errorMessage);
+}
---
+++ tests/Frameworks/FunctionalTest/Results/FailureTest.cs
@@ -0,0 +1,94 @@
+using Hermes.Frameworks.Functional.Results;
+using Xunit;
+
+namespace Hermes.Frameworks.FunctionalTest.Results;
+
+public class FailureTest
+{
+    private const string TestValue = nameof(FailureTest);
+
+    [Fact]
+    public void Constructor_Null_ThrowArgumentNullException()
+    {
+        // Arrange & Act
+        Action sut = () => new Failure<string>(null!);
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(sut);
+    }
+
+    [Fact]
+    public void ErrorCode_NotNull_ExpectedCode()
+    {
+        // Arrange
+        var sut = new Failure<string>(new ErrorMessage(TestValue));
+
+        // Act
+        var (code, data) = sut.ErrorMessage;
+
+        // Assert
+        Assert.Equal(TestValue, code);
+        Assert.Empty(data);
+    }
+
+    [Fact]
+    public void ImplicitCastToErrorMessage_FailureObject_ErrorMessage()
+    {
+        // Arrange
+        var sut = new Failure<string>(new ErrorMessage(TestValue));
+
+        // Act
+        ErrorMessage actual = sut;
+
+        // Assert
+        // Assert
+        Assert.Equal(TestValue, actual.Code);
+        Assert.Empty(actual.Data);
+    }
+
+    [Fact]
+    public void ImplicitCastToFailure_ErrorMessage_Failure()
+    {
+        // Arrange
+        Failure<string> sut = new ErrorMessage(TestValue);
+
+        // Act
+        var (code, data) = sut.ErrorMessage;
+
+        // Assert
+        Assert.Equal(TestValue, code);
+        Assert.Empty(data);
+    }
+
+    [Fact]
+    public void Match_AlwaysOnFailure()
+    {
+        // Arrange
+        var sut = new Failure<string>(new ErrorMessage(TestValue));
+
+        // Act
+        var actual = sut.Match(
+            onSuccess: success => string.Empty,
+            onFailure: failure => failure.Code
+        );
+
+        // Assert
+        Assert.Equal(TestValue, actual);
+    }
+
+    [Fact]
+    public async Task MatchAsync_AlwaysOnFailure()
+    {
+        // Arrange
+        var sut = new Failure<string>(new ErrorMessage(TestValue));
+
+        // Act
+        var actual = await sut.MatchAsync(
+            onSuccess: async _ => await Task.Run(() => string.Empty),
+            onFailure: failure => failure.Code
+        );
+
+        // Assert
+        Assert.Equal(TestValue, actual);
+    }
+}
