6851464d9ceef05c66e56d72305609d5b3cdf629
Spec <dawidjachowicz@proton.me>
2022-11-10 06:53:28 +0100

    Add IReadOnlyListRepository interface

---
+++ src/Frameworks/Repositories/ReadOnly/IReadOnlyListRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace Hermes.Frameworks.Repositories.ReadOnly;
+
+public interface IReadOnlyListRepository<TEntity>
+{
+    Task<long> GetCountAsync(CancellationToken cancellationToken = default);
+
+    Task<List<TEntity>> GetListAsync(bool includeDetails = false, CancellationToken cancellationToken = default);
+
+    Task<List<TEntity>> GetListAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        bool includeDetails = false,
+        CancellationToken cancellationToken = default);
+
+    Task<List<TEntity>> GetPagedListAsync(
+        int skipCount,
+        int maxResultCount,
+        string sorting,
+        bool includeDetails = false,
+        CancellationToken cancellationToken = default);
+}
