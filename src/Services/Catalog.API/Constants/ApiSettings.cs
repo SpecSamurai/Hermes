4cdfa2697a71b871aa954420e9b12a62b9fea193
Spec <dawidjachowicz@proton.me>
2022-11-13 11:31:24 +0100

    Add endpoint names to ApiSettings

--- src/Services/Catalog.API/Constants/ApiSettings.cs
+++ src/Services/Catalog.API/Constants/ApiSettings.cs
@@ -3,6 +3,11 @@ namespace Hermes.Catalog.API.Constants;
 public static class ApiSettings
 {
     public static readonly string ApiTitle = typeof(ApiSettings).Assembly.GetName().Name ?? string.Empty;
+    public const string ApiPrefix = "api";
     public const string ApiVersion1 = "v1";
     public const string ApiVersion2 = "v2";
+
+    public const string ItemsEndpoint = "items";
+    public const string BrandsEndpoint = "brands";
+    public const string ItemTypesEndpoint = "itemtypes";
 }
