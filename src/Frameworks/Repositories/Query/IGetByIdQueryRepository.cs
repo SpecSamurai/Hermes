17baeaba5f446b4f017ffbdccc5604256b7393bb
Spec <dawidjachowicz@proton.me>
2022-11-17 13:03:23 +0100

    Remove nullable FindAsync method from IGetByIdQueryRepository

--- src/Frameworks/Repositories/Query/IGetByIdQueryRepository.cs
+++ src/Frameworks/Repositories/Query/IGetByIdQueryRepository.cs
@@ -2,6 +2,8 @@ namespace Hermes.Frameworks.Repositories.Query;
 
 public interface IGetByIdQueryRepository<TKey, TEntity>
 {
-    Task<TEntity> GetAsync(TKey id, bool includeDetails = true, CancellationToken cancellationToken = default);
-    Task<TEntity?> FindAsync(TKey id, bool includeDetails = true, CancellationToken cancellationToken = default);
+    Task<TEntity> GetAsync(
+        TKey id,
+        bool includeDetails = true,
+        CancellationToken cancellationToken = default);
 }
