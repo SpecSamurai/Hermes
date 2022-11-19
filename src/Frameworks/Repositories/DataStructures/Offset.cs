547f43307ff2ab519b919b4c1bce76e389fe1371
Spec <dawidjachowicz@proton.me>
2022-11-19 16:35:43 +0100

    Add factory method to Offset record

--- src/Frameworks/Repositories/DataStructures/Offset.cs
+++ src/Frameworks/Repositories/DataStructures/Offset.cs
@@ -4,4 +4,11 @@ public record Offset
 {
     public required int Skip { get; init; }
     public required int Take { get; init; }
+
+    public static Offset Of(int pageIndex, int pageSize) =>
+        new()
+        {
+            Skip = pageIndex * pageSize,
+            Take = pageSize
+        };
 }
