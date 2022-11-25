a9c9ba0b609aefaccbd9a5827eda2e7d80b73204
Spec <dawidjachowicz@proton.me>
2022-11-25 22:24:33 +0100

    Add IListByPredicateQueryRepository definition

---
+++ src/Frameworks/Repositories/Query/IListByPredicateQueryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Hermes.Frameworks.Repositories.Query;
+
+public interface IListByPredicateQueryRepository<TEntity>
+{
+    Task<List<TResult>> GetListAsync<TResult>(
+        Expression<Func<TEntity, bool>> predicate,
+        Expression<Func<TEntity, TResult>> selector,
+        bool includeDetails = false,
+        CancellationToken cancellationToken = default);
+}
