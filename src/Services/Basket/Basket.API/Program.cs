4f3b4f604ee2886c5ab22399a42df07fdcd3c5b8
Spec <dawidjachowicz@proton.me>
2022-11-29 07:20:40 +0100

    Configure endpoints and swagger routing for Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,47 @@
+using Hermes.Basket.API.Constants;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.User.json", optional: true, reloadOnChange: true);
+}
+
+builder.Services
+    .AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SupportNonNullableReferenceTypes();
+
+    options.SwaggerDoc(
+        ApiSettings.ApiVersion1,
+        new OpenApiInfo
+        {
+            Title = ApiSettings.ApiTitle,
+            Version = ApiSettings.ApiVersion1
+        });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.RoutePrefix = string.Empty;
+    options.SwaggerEndpoint(
+        $"/swagger/{ApiSettings.ApiVersion1}/swagger.json",
+        $"{ApiSettings.ApiTitle} {ApiSettings.ApiVersion1}");
+});
+
+app.MapControllers();
+app.MapSwagger();
 
 app.Run();
