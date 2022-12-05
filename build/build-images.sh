197df5f0cb29432c0a52e7f3118c5d6d2dc36a7f
Spec <dawidjachowicz@proton.me>
2022-12-05 20:33:10 +0100

    Improve build-images script to use parent folder as image names

--- build/build-images.sh
+++ build/build-images.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+tag="latest"
 net_version="7.0"
 context=$(pwd)
 dockerfiles=(`find . -name Dockerfile`)
 
 for dockerfile in "${dockerfiles[@]}"
 do
+    parentDir=$(basename $(dirname "$dockerfile"))
+    parentDir=${parentDir//./-}
+    parentDir="$(tr [A-Z] [a-z] <<< "$parentDir")"
+
     docker build \
-        --tag hermes/catalog-api:latest \
+        --tag "hermes/$parentDir:$tag" \
         --build-arg NET_VERSION="$net_version" \
         --build-arg CONFIGURATION=Debug \
         --file "$dockerfile" \
