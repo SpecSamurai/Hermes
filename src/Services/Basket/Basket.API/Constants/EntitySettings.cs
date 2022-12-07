57c9a42c613677cd215b216a4e1fc61ffdbba94a
Spec <dawidjachowicz@proton.me>
2022-12-07 19:35:06 +0100

    Add EntitySettings

---
+++ src/Services/Basket/Basket.API/Constants/EntitySettings.cs
@@ -0,0 +1,10 @@
+namespace Hermes.Basket.API.Constants;
+
+public static class EntitySettings
+{
+    public static class CustomerBasket
+    {
+        public const string Prefix = "CustomerBasket";
+        public const string IndexName = "CustomerBaskets";
+    }
+}
