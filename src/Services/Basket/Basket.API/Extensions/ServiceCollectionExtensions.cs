b33e28b22492ece9473bfa7995f8fb55bf13b3f6
Spec <dawidjachowicz@proton.me>
2022-12-07 19:51:25 +0100

    Change ServiceCollectionExtensions - Register RedisConnectionProvider - Add AddDependencies method and register repositories

--- src/Services/Basket/Basket.API/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/Basket/Basket.API/Extensions/ServiceCollectionExtensions.cs
@@ -1,20 +1,27 @@
 using Hermes.Basket.API.Constants;
+using Hermes.Basket.API.Repositories;
+using Redis.OM;
 using StackExchange.Redis;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddRedisConnectionMultiplexer(this IServiceCollection serviceCollection)
+    public static IServiceCollection AddRedisConnectionProvider(this IServiceCollection serviceCollection)
     {
-        return serviceCollection.AddSingleton<IConnectionMultiplexer>(serviceProvider =>
+        return serviceCollection.AddSingleton<RedisConnectionProvider>(serviceProvider =>
         {
             var connectionString = serviceProvider
                 .GetRequiredService<IConfiguration>()
-                .GetConnectionString("Redis")
+                .GetConnectionString("BasketDatabase")
                 ?? throw new InvalidOperationException(ErrorCodes.Redis.InvalidConnectionString);
 
-            return ConnectionMultiplexer.Connect(connectionString);
+            return new RedisConnectionProvider(ConnectionMultiplexer.Connect(connectionString));
         });
     }
+
+    public static IServiceCollection AddDependencies(this IServiceCollection serviceCollection) =>
+        serviceCollection
+            .AddScoped<IBasketQueryRepository, BasketQueryRepository>()
+            .AddScoped<IBasketCommandRepository, BasketCommandRepository>();
 }
