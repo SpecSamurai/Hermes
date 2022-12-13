7e7b312b9d0d84b8440c58519d0a3609ed8a6c7f
Spec <dawidjachowicz@proton.me>
2022-12-13 18:12:19 +0100

    Extend DomainException to handle inner exception

--- src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
+++ src/Services/Ordering/Ordering.Domain/Exceptions/DomainException.cs
@@ -2,10 +2,15 @@ namespace Hermes.Ordering.Domain.Exceptions;
 
 public class DomainException : Exception
 {
-    public DomainException(string code, params (string Key, object Value)[] parameters)
-        : base(code)
+    public DomainException(string code, Exception? innerException, params (string Key, object Value)[] parameters)
+        : base(code, innerException)
     {
         foreach (var parameter in parameters)
             Data.Add(parameter.Key, parameter.Value);
+    }
+
+    public DomainException(string code, params (string Key, object Value)[] parameters)
+        : this(code, default, parameters)
+    {
     }
 }
