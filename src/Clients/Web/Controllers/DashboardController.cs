df8857c47f12092d0fa9745462327b3faf50c44f
Spec <dawidjachowicz@proton.me>
2022-12-19 20:50:24 +0100

    Add Web client initial project

---
+++ src/Clients/Web/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Client.Web.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
