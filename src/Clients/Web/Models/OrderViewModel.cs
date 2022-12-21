4e1768142d98ba73487e18ac2353cb37f0e2ef6f
Spec <dawidjachowicz@proton.me>
2022-12-21 23:07:42 +0100

    Add OrderViewModel

---
+++ src/Clients/Web/Models/OrderViewModel.cs
@@ -0,0 +1,10 @@
+namespace Hermes.Client.Web.Models;
+
+internal class OrderViewModel
+{
+    public required Guid Id { get; init; }
+    public required IEnumerable<OrderedProductViewModel> Products { get; init; }
+    public required decimal Price { get; init; }
+    public required DateTime Date { get; init; }
+    public required string Status { get; init; }
+}
