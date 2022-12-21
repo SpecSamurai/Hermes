4fe5cc065da7c68786c7d9236593903d2a311eab
Spec <dawidjachowicz@proton.me>
2022-12-21 23:16:59 +0100

    Add PaymentsController

---
+++ src/Clients/Web/Controllers/PaymentsController.cs
@@ -0,0 +1,41 @@
+﻿using Hermes.Client.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Client.Web.Controllers;
+
+public class PaymentsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View(new PaymentsViewModel
+        {
+            Payments = new List<PaymentViewModel>
+            {
+                new PaymentViewModel
+                {
+                    Id = Guid.NewGuid(),
+                    OrderId = Guid.NewGuid(),
+                    Price = 100,
+                    Date = DateTime.Now,
+                    Status = "Paid"
+                },
+                new PaymentViewModel
+                {
+                    Id = Guid.NewGuid(),
+                    OrderId = Guid.NewGuid(),
+                    Price = 100,
+                    Date = DateTime.Now,
+                    Status = "Paid"
+                },
+                new PaymentViewModel
+                {
+                    Id = Guid.NewGuid(),
+                    OrderId = Guid.NewGuid(),
+                    Price = 100,
+                    Date = DateTime.Now,
+                    Status = "Paid"
+                },
+            }
+        });
+    }
+}
