34723491b913510ab407f6ce71921e21740bb9b9
Spec <dawidjachowicz@proton.me>
2022-12-21 23:06:53 +0100

    Add ProductViewModel

---
+++ src/Clients/Web/Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+namespace Hermes.Client.Web.Models;
+
+internal class ProductViewModel
+{
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    public required decimal Price { get; init; }
+    public required string PictureUri { get; init; }
+    public required string Brand { get; init; }
+}
