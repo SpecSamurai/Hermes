c8c8b9382af6751188888c83e5b9f8103c6b37eb
Spec <dawidjachowicz@proton.me>
2022-12-21 23:16:36 +0100

    Adjust CatalogController to return view models

--- src/Clients/Web/Controllers/CatalogController.cs
+++ src/Clients/Web/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hermes.Client.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hermes.Client.Web.Controllers;
 
@@ -6,6 +7,51 @@ public class CatalogController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        return View(new CatalogViewModel
+        {
+            Products = new List<ProductViewModel>
+            {
+                new ProductViewModel
+                {
+                    Name = "Shirt 1",
+                    Description = "Blue Denim Shirt",
+                    PictureUri = @"https://via.placeholder.com/200",
+                    Brand = "Brand 1",
+                    Price = 10
+                },
+                new ProductViewModel
+                {
+                    Name = "Shirt 2",
+                    Description = "Blue Denim Shirt",
+                    PictureUri = @"https://via.placeholder.com/200",
+                    Brand = "Brand 2",
+                    Price = 12
+                },
+                new ProductViewModel
+                {
+                    Name = "Shirt 3",
+                    Description = "Blue Denim Shirt",
+                    PictureUri = @"https://via.placeholder.com/200",
+                    Brand = "Brand 3",
+                    Price = 10
+                },
+                new ProductViewModel
+                {
+                    Name = "Shirt 4",
+                    Description = "Blue Denim Shirt",
+                    PictureUri = @"https://via.placeholder.com/200",
+                    Brand = "Brand 4",
+                    Price = 9
+                },
+                new ProductViewModel
+                {
+                    Name = "Shirt 5",
+                    Description = "Blue Denim Shirt",
+                    PictureUri = @"https://via.placeholder.com/200",
+                    Brand = "Brand 5",
+                    Price = 13
+                },
+            }
+        });
     }
 }
