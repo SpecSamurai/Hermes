8fcb18718ff24a18e051f4a1fec8105a8eb1831e
Spec <dawidjachowicz@proton.me>
2022-12-31 06:53:31 +0100

    Add example JS trigger to handle notification toasts

--- src/Clients/Web/wwwroot/js/site.js
+++ src/Clients/Web/wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿
+﻿const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementsByClassName('toast')
+if (toastTrigger) {
+    toastTrigger.addEventListener('click', () => {
+        for (const toastElement of toastLiveExample) {
+            const toast = new bootstrap.Toast(toastElement)
+            toast.show()
+        }
+    })
+}
