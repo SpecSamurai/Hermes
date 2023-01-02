6e6f7d5e4a55b62a538128a0a1c271189592c972
Spec <dawidjachowicz@proton.me>
2023-01-02 07:16:38 +0100

    Add ToMoney extension method

--- src/Frameworks/Functional/Fintech/ExpressionExtensions.cs
+++ src/Frameworks/Functional/Fintech/ExpressionExtensions.cs
@@ -4,4 +4,7 @@ public static class ExpressionExtensions
 {
     public static IExpression Plus(this IExpression augend, IExpression addend) =>
         new Sum(augend, addend);
+
+    public static Money ToMoney(this decimal amount, Currency currency) =>
+        new Money(amount, currency);
 }
