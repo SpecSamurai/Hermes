e474da1eb7f1c04ee7b04e2e313d0d88bc67e3cc
Spec <dawidjachowicz@proton.me>
2023-01-14 07:04:38 +0100

    Remove Plus Operator from Money unit tests

--- tests/Frameworks/FunctionalTest/Fintech/MoneyTest.cs
+++ tests/Frameworks/FunctionalTest/Fintech/MoneyTest.cs
@@ -5,8 +5,8 @@ namespace Hermes.Frameworks.FunctionalTest.Fintech;
 
 public class MoneyTest
 {
-    private const Currency FromTest = Currency.USD;
-    private const Currency ToTest = Currency.EUR;
+    private readonly Currency FromTest = Currency.USD;
+    private readonly Currency ToTest = Currency.EUR;
 
     [Fact]
     public void Reduce_USD_EUR()
@@ -21,21 +21,5 @@ public class MoneyTest
         // Assert
         Assert.Equal(3.0m, actual.Amount);
         Assert.Equal(ToTest, actual.Currency);
-    }
-
-    [Fact]
-    public void OperatorPlus_Money_Sum()
-    {
-        // Arrange
-        var exchangeRates = new ExchangeRates { { (FromTest, ToTest), 1.5m } };
-        var sut1 = new Money(1, Currency.USD);
-        var sut2 = new Money(2, Currency.EUR);
-
-        // Act
-        var actual = (Sum)(sut1 + sut2);
-
-        // Assert
-        Assert.Equal(actual.Left, sut1);
-        Assert.Equal(actual.Right, sut2);
     }
 }
