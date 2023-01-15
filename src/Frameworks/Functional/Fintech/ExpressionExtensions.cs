55fb27c5e8712fe158724e44658e8bbb42f75810
Spec <dawidjachowicz@proton.me>
2023-01-15 07:20:59 +0100

    Add new Expression extension methods

--- src/Frameworks/Functional/Fintech/ExpressionExtensions.cs
+++ src/Frameworks/Functional/Fintech/ExpressionExtensions.cs
@@ -2,9 +2,15 @@ namespace Hermes.Frameworks.Functional.Fintech;
 
 public static class ExpressionExtensions
 {
-    public static IExpression Plus(this IExpression augend, IExpression addend) =>
-        new Sum(augend, addend);
+    public static IExpression Add(this IExpression left, IExpression right) =>
+        new Sum(left, right);
 
-    public static Money ToMoney(this decimal amount, Currency currency) =>
-        new Money(amount, currency);
+    public static IExpression Subtract(this IExpression left, IExpression right) =>
+        new Difference(left, right);
+
+    public static IExpression Multiply(this IExpression left, IExpression right) =>
+        new Product(left, right);
+
+    public static IExpression Divide(this IExpression left, IExpression right) =>
+        new Fraction(left, right);
 }
