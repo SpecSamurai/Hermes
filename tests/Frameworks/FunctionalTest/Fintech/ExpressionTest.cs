0195db08bda05e8d79d132eeec4a100f7ed88edf
Spec <dawidjachowicz@proton.me>
2023-01-15 07:21:17 +0100

    Add Expression unit tests

---
+++ tests/Frameworks/FunctionalTest/Fintech/ExpressionTest.cs
@@ -0,0 +1,76 @@
+using Hermes.Frameworks.Functional.Fintech;
+using Xunit;
+
+namespace Hermes.Frameworks.FunctionalTest.Fintech;
+
+public class ExpressionTest
+{
+    private static readonly ExchangeRates exchangeRates = new ExchangeRates {
+        { (Currency.USD, Currency.GBP), 0.8m },
+        { (Currency.EUR, Currency.GBP), 1.2m }
+    };
+
+    [Fact]
+    public void Reduce_AddExpressions_Sum()
+    {
+        // Arrange
+        var usd = new Money(1, Currency.USD);
+        var eur = new Money(2, Currency.EUR);
+        var sut = usd.Add(eur);
+
+        // Act
+        var actual = sut.Reduce(exchangeRates, Currency.GBP);
+
+        // Assert
+        Assert.Equal(3.2m, actual.Amount);
+        Assert.Equal(Currency.GBP, actual.Currency);
+    }
+
+    [Fact]
+    public void Reduce_SubtractExpressions_Difference()
+    {
+        // Arrange
+        var usd = new Money(1, Currency.USD);
+        var eur = new Money(2, Currency.EUR);
+        var sut = usd.Subtract(eur);
+
+        // Act
+        var actual = sut.Reduce(exchangeRates, Currency.GBP);
+
+        // Assert
+        Assert.Equal(-1.6m, actual.Amount);
+        Assert.Equal(Currency.GBP, actual.Currency);
+    }
+
+    [Fact]
+    public void Reduce_MultiplyExpressions_Product()
+    {
+        // Arrange
+        var usd = new Money(1, Currency.USD);
+        var eur = new Money(2, Currency.EUR);
+        var sut = usd.Multiply(eur);
+
+        // Act
+        var actual = sut.Reduce(exchangeRates, Currency.GBP);
+
+        // Assert
+        Assert.Equal(1.92m, actual.Amount);
+        Assert.Equal(Currency.GBP, actual.Currency);
+    }
+
+    [Fact]
+    public void Reduce_DivideExpressions_Fraction()
+    {
+        // Arrange
+        var usd = new Money(1, Currency.USD);
+        var eur = new Money(2, Currency.EUR);
+        var sut = usd.Divide(eur);
+
+        // Act
+        var actual = sut.Reduce(exchangeRates, Currency.GBP);
+
+        // Assert
+        Assert.Equal(0.3333m, actual.Amount, precision: 4);
+        Assert.Equal(Currency.GBP, actual.Currency);
+    }
+}
