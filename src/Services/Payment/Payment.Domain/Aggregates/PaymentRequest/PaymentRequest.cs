0b2a4163f706de2c3fcf6a04eb9d51f2d1f6873c
Spec <dawidjachowicz@proton.me>
2023-01-24 17:59:17 +0100

    feature(payment): Add PaymentRequest entity to Payment Domain

---
+++ src/Services/Payment/Payment.Domain/Aggregates/PaymentRequest/PaymentRequest.cs
@@ -0,0 +1,43 @@
+﻿using Hermes.Frameworks.DDD.Entities;
+using Hermes.Payment.Domain.Events;
+
+namespace Hermes.Payment.Domain.Aggregates.PaymentRequest;
+
+public class PaymentRequest : AggregateRoot<Guid>
+{
+    public PaymentRequest(
+        Guid id,
+        Guid orderId,
+        Guid buyerId,
+        PaymentRequestState state,
+        IEnumerable<PaymentRequestProduct> products) : base(id)
+    {
+        OrderId = orderId;
+        BuyerId = buyerId;
+        State = state;
+        Products = products;
+    }
+
+    public Guid OrderId { get; }
+    public Guid BuyerId { get; }
+    public PaymentRequestState State { get; private set; }
+    public IEnumerable<PaymentRequestProduct> Products { get; }
+
+    public void SetAsCompleted()
+    {
+        if (State != PaymentRequestState.Completed)
+        {
+            State = PaymentRequestState.Completed;
+            AddDistributedEvent(new PaymentRequestCompletedDomainEvent { PaymentRequestId = Id });
+        }
+    }
+
+    public void SetAsFailed()
+    {
+        if (State != PaymentRequestState.Failed)
+        {
+            State = PaymentRequestState.Failed;
+            AddDistributedEvent(new PaymentRequestFailedDomainEvent { PaymentRequestId = Id });
+        }
+    }
+}
