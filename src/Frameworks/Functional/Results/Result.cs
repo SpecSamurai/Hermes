d023cad950236ee288311c4bf63e896e1b247224
Spec <dawidjachowicz@proton.me>
2023-01-26 07:12:02 +0100

    refactor(functional): Refactor Result static factory methods
    Change factories to return specific implementation instead of the IResult interface.

--- src/Frameworks/Functional/Results/Result.cs
+++ src/Frameworks/Functional/Results/Result.cs
@@ -2,13 +2,13 @@ namespace Hermes.Frameworks.Functional.Results;
 
 public static class Result
 {
-    public static IResult<TValue> Success<TValue>(TValue success) =>
-        new Success<TValue>(success);
+    public static Success<TValue> Success<TValue>(TValue success) =>
+        success;
 
-    public static IResult<TValue> Failure<TValue>(ErrorMessage failure) =>
-        new Failure<TValue>(failure);
+    public static Failure<TValue> Failure<TValue>(ErrorMessage failure) =>
+        failure;
 
-    public static IResult<TValue> Failure<TValue>(string code, params (string Key, object Value)[] parameters) =>
+    public static Failure<TValue> Failure<TValue>(string code, params (string Key, object Value)[] parameters) =>
         new Failure<TValue>(
             new ErrorMessage(code, parameters));
 }
