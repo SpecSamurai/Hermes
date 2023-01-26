3d18d7ec29a4d3a52d713c1caea6743a7634abd9
Spec <dawidjachowicz@proton.me>
2023-01-26 07:14:24 +0100

    feature(functional): Change ResultExtensions class to be partial and add new extension methods
    Add ToSuccess factory method and GetValueOrFallback.

--- src/Frameworks/Functional/Results/ResultExtensions.cs
+++ src/Frameworks/Functional/Results/ResultExtensions.cs
@@ -1,10 +1,16 @@
 namespace Hermes.Frameworks.Functional.Results;
 
-public static class ResultExtensions
+public static partial class ResultExtensions
 {
+    public static Success<TValue> ToSuccess<TValue>(this TValue success) =>
+        success;
+
     public static bool IsSuccess<TValue>(this IResult<TValue> result) =>
         result.Match(onSuccess: _ => true, onFailure: _ => false);
 
     public static bool IsFailure<TValue>(this IResult<TValue> result) =>
         result.Match(onSuccess: _ => false, onFailure: _ => true);
+
+    public static TValue GetValueOrFallback<TValue>(this IResult<TValue> result, TValue fallback) =>
+        result.Match(onSuccess: value => value, onFailure: _ => fallback);
 }
