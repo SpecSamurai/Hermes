0b49bea26163b4c3df08036806ec19827c5ae374
Spec <dawidjachowicz@proton.me>
2023-02-04 15:01:50 +0100

    feature(client): Add Resources keys

---
+++ src/Clients/Web/Constants/Resources.cs
@@ -0,0 +1,15 @@
+namespace Hermes.Client.Web.Constants;
+
+public static class Resources
+{
+    public static class Pagination
+    {
+        public const string PreviousButtonLabel = $"{nameof(Pagination)}|{nameof(PreviousButtonLabel)}";
+        public const string NextButtonLabel = $"{nameof(Pagination)}|{nameof(NextButtonLabel)}";
+    }
+
+    public static class Catalog
+    {
+        public const string CardLinkLabel = $"{nameof(Catalog)}|{nameof(CardLinkLabel)}";
+    }
+}
