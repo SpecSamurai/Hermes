a270eb726dce0ba44a36d12a49eb62dc1bb2bd56
Spec <dawidjachowicz@proton.me>
2023-02-05 09:32:46 +0100

    feature(client): Add new Resources
    Add Resources for Navigation, Login, Orders and Payments.
    Refactor Catalog Resources.

--- src/Clients/Web/Constants/Resources.cs
+++ src/Clients/Web/Constants/Resources.cs
@@ -2,6 +2,17 @@ namespace Hermes.Client.Web.Constants;
 
 public static class Resources
 {
+    public static class Navigation
+    {
+        public const string OrdersLabel = $"{nameof(Navigation)}|{nameof(OrdersLabel)}";
+        public const string PaymentsLabel = $"{nameof(Navigation)}|{nameof(PaymentsLabel)}";
+    }
+
+    public static class Login
+    {
+        public const string LoginButtonLabel = $"{nameof(Login)}|{nameof(LoginButtonLabel)}";
+    }
+
     public static class Pagination
     {
         public const string PreviousButtonLabel = $"{nameof(Pagination)}|{nameof(PreviousButtonLabel)}";
@@ -10,6 +21,27 @@ public static class Resources
 
     public static class Catalog
     {
-        public const string CardLinkLabel = $"{nameof(Catalog)}|{nameof(CardLinkLabel)}";
+        public const string Title = $"{nameof(Catalog)}|{nameof(Title)}";
+        public const string AddToBasketButtonLabel = $"{nameof(Catalog)}|{nameof(AddToBasketButtonLabel)}";
+    }
+
+    public static class Orders
+    {
+        public const string Title = $"{nameof(Orders)}|{nameof(Title)}";
+
+        public const string ProductsColumnHeader = $"{nameof(Orders)}|{nameof(ProductsColumnHeader)}";
+        public const string PriceColumnHeader = $"{nameof(Orders)}|{nameof(PriceColumnHeader)}";
+        public const string DateColumnHeader = $"{nameof(Orders)}|{nameof(DateColumnHeader)}";
+        public const string StatusColumnHeader = $"{nameof(Orders)}|{nameof(StatusColumnHeader)}";
+    }
+
+    public static class Payments
+    {
+        public const string Title = $"{nameof(Payments)}|{nameof(Title)}";
+
+        public const string OrderColumnHeader = $"{nameof(Payments)}|{nameof(OrderColumnHeader)}";
+        public const string PriceColumnHeader = $"{nameof(Payments)}|{nameof(PriceColumnHeader)}";
+        public const string DateColumnHeader = $"{nameof(Payments)}|{nameof(DateColumnHeader)}";
+        public const string StatusColumnHeader = $"{nameof(Payments)}|{nameof(StatusColumnHeader)}";
     }
 }
