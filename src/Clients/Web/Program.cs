5680ba24a4354555dc43faa6121d6c83ebb0351e
Spec <dawidjachowicz@proton.me>
2023-02-07 12:41:37 +0100

    feature(client): Configure Options and services
    - Add appsettings.User.json to configuration sources
    - Register CatalogApiOptions and PaginationOptions
    - Add View Localization services
    - Register Catalog, Orders, Payment, Basket services
    - Register Catalog.API HTTP Client

--- src/Clients/Web/Program.cs
+++ src/Clients/Web/Program.cs
@@ -1,6 +1,49 @@
+using Hermes.Client.Web.Constants;
+using Hermes.Client.Web.Options;
+using Hermes.Client.Web.Services;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.User.json", optional: true, reloadOnChange: true);
+}
+
+builder.Services
+    .AddOptions<CatalogApiOptions>()
+    .Configure<IConfiguration>((settings, configuration) =>
+        configuration
+            .GetSection(nameof(CatalogApiOptions))
+            .Bind(settings))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+builder.Services
+    .AddOptions<PaginationOptions>()
+    .Configure<IConfiguration>((settings, configuration) =>
+        configuration
+            .GetSection(nameof(PaginationOptions))
+            .Bind(settings))
+    .ValidateDataAnnotations()
+    .ValidateOnStart();
+
+builder.Services
+    .AddControllersWithViews()
+    .AddViewLocalization();
+
+builder.Services
+    .AddHttpClient(Settings.CatalogApiClientName, (serviceProvider, client) =>
+    {
+        var options = serviceProvider.GetRequiredService<IOptions<CatalogApiOptions>>().Value;
+        client.BaseAddress = new Uri(options.BaseAddress);
+    });
+
+builder.Services
+    .AddScoped<ICatalogService, CatalogService>()
+    .AddScoped<IOrdersService, OrdersService>()
+    .AddScoped<IPaymentsService, PaymentsService>()
+    .AddScoped<IBasketService, BasketService>();
 
 var app = builder.Build();
 
