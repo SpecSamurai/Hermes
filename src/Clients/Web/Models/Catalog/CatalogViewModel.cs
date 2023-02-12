3a3eb041f9fe35ac60d4879f793e744572caceca
Spec <dawidjachowicz@proton.me>
2023-02-12 15:16:27 +0100

    feature(client): Add Catalog view model

---
+++ src/Clients/Web/Models/Catalog/CatalogViewModel.cs
@@ -0,0 +1,9 @@
+using Hermes.Client.Web.Models.Shared;
+
+namespace Hermes.Client.Web.Models.Catalog;
+
+internal class CatalogViewModel
+{
+    public required PaginationViewModel Pagination { get; init; }
+    public required IEnumerable<ProductViewModel> Products { get; init; }
+}
