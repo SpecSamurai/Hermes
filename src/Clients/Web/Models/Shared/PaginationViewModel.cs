a512eed4d7e74b499a86fa0b13fe58bfe94d58d1
Spec <dawidjachowicz@proton.me>
2023-02-13 22:36:41 +0100

    feature(client): Adjust Pagination view model according to the new view

--- src/Clients/Web/Models/Shared/PaginationViewModel.cs
+++ src/Clients/Web/Models/Shared/PaginationViewModel.cs
@@ -4,9 +4,6 @@ public class PaginationViewModel
 {
     public required int PageIndex { get; init; }
     public required int TotalPages { get; init; }
-    public required int NextPagesCount { get; init; }
-
+    public required int MaxAdditionalPages { get; init; }
     public required string PageUrl { get; init; }
-    public required string PreviousButtonLabel { get; init; }
-    public required string NextButtonLabel { get; init; }
 }
