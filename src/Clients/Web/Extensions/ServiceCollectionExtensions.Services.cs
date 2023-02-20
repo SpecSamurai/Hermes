64094be47c2d6b6ae7bafa6d27759252c253f6c6
Spec <dawidjachowicz@proton.me>
2023-02-20 21:37:58 +0100

    feature(client): Add ServiceCollection extension method which registers Services
    Register Catalog, Orders, Payments and Basket services.

---
+++ src/Clients/Web/Extensions/ServiceCollectionExtensions.Services.cs
@@ -0,0 +1,15 @@
+using Hermes.Client.Web.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class ServiceCollectionExtensions
+{
+    public static void RegisterServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddScoped<ICatalogService, CatalogService>()
+            .AddScoped<IOrdersService, OrdersService>()
+            .AddScoped<IPaymentsService, PaymentsService>()
+            .AddScoped<IBasketService, BasketService>();
+    }
+}
