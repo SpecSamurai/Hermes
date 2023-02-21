cc66f52e216e802104d1c522d58cb3df7e1fc23d
Spec <dawidjachowicz@proton.me>
2023-02-21 23:20:35 +0100

    feature(client): Add new HttpClient constnats

---
+++ src/Clients/Web/Constants/HttpClients.cs
@@ -0,0 +1,8 @@
+namespace Hermes.Client.Web.Constants;
+
+public static class HttpClients
+{
+    public const string CatalogApi = "Catalog.API";
+    public const string OrderingApi = "Ordering.API";
+    public const string PaymentApi = "Payment.API";
+}
--- src/Clients/Web/Constants/Settings.cs
+++
@@ -1,6 +0,0 @@
-namespace Hermes.Client.Web.Constants;
-
-public static class Settings
-{
-    public const string CatalogApiClientName = "Catalog.API";
-}
