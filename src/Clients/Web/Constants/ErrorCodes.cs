a80fed81336f5fc22bb37a80507427e98577ebc7
Spec <dawidjachowicz@proton.me>
2023-02-22 11:46:59 +0100

    feature(client): Add Ordering and Payment error codes

--- src/Clients/Web/Constants/ErrorCodes.cs
+++ src/Clients/Web/Constants/ErrorCodes.cs
@@ -8,4 +8,14 @@ internal static class ErrorCodes
     {
         public const string GetPageFailed = $"{nameof(Catalog)}|{nameof(GetPageFailed)}";
     }
+
+    internal static class Ordering
+    {
+        public const string GetPageFailed = $"{nameof(Ordering)}|{nameof(GetPageFailed)}";
+    }
+
+    internal static class Payment
+    {
+        public const string GetPageFailed = $"{nameof(Payment)}|{nameof(GetPageFailed)}";
+    }
 }
