610aa9b5d9b030dfe2626ff5125329ccfa8b0e5e
Spec <dawidjachowicz@proton.me>
2023-02-23 13:41:27 +0100

    refactor(client): Refactor main Program and adjust it to use new extension methods
    Register all dependencies and configuration.

--- src/Clients/Web/Program.cs
+++ src/Clients/Web/Program.cs
@@ -1,8 +1,3 @@
-using Hermes.Client.Web.Constants;
-using Hermes.Client.Web.Options;
-using Hermes.Client.Web.Services;
-using Microsoft.Extensions.Options;
-
 var builder = WebApplication.CreateBuilder(args);
 
 if (builder.Environment.IsDevelopment())
@@ -10,40 +5,13 @@ if (builder.Environment.IsDevelopment())
     builder.Configuration.AddJsonFile("appsettings.User.json", optional: true, reloadOnChange: true);
 }
 
-builder.Services
-    .AddOptions<CatalogApiOptions>()
-    .Configure<IConfiguration>((settings, configuration) =>
-        configuration
-            .GetSection(nameof(CatalogApiOptions))
-            .Bind(settings))
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-builder.Services
-    .AddOptions<PaginationOptions>()
-    .Configure<IConfiguration>((settings, configuration) =>
-        configuration
-            .GetSection(nameof(PaginationOptions))
-            .Bind(settings))
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
+builder.Services.RegisterOptions();
+builder.Services.RegisterHttpClients();
+builder.Services.RegisterServices();
 
 builder.Services
     .AddControllersWithViews()
     .AddViewLocalization();
-
-builder.Services
-    .AddHttpClient(Settings.CatalogApiClientName, (serviceProvider, client) =>
-    {
-        var options = serviceProvider.GetRequiredService<IOptions<CatalogApiOptions>>().Value;
-        client.BaseAddress = new Uri(options.BaseAddress);
-    });
-
-builder.Services
-    .AddScoped<ICatalogService, CatalogService>()
-    .AddScoped<IOrdersService, OrdersService>()
-    .AddScoped<IPaymentsService, PaymentsService>()
-    .AddScoped<IBasketService, BasketService>();
 
 var app = builder.Build();
 
