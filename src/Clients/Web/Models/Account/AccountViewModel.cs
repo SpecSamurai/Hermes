ce52c5e334c508f554ad4caa7caea70b67e22977
Spec <dawidjachowicz@proton.me>
2023-08-10 19:32:35 +0200

    feature(client): Add AccountViewModel view model

---
+++ src/Clients/Web/Models/Account/AccountViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hermes.Client.Web.Models.Account;
+
+internal class AccountViewModel
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public required IEnumerable<string> Roles { get; init; }
+}
