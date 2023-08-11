0c91b82672d56534957fc5479b19740bc095028a
Spec <dawidjachowicz@proton.me>
2023-08-11 17:29:53 +0200

    feature(client): Add Resources constants

--- src/Clients/Web/Constants/Resources.cs
+++ src/Clients/Web/Constants/Resources.cs
@@ -2,27 +2,38 @@ namespace Hermes.Client.Web.Constants;
 
 public static class Resources
 {
-    public static class Navigation
+    public static class Account
     {
-        public const string OrdersLabel = $"{nameof(Navigation)}|{nameof(OrdersLabel)}";
-        public const string PaymentsLabel = $"{nameof(Navigation)}|{nameof(PaymentsLabel)}";
-    }
+        public const string Title = $"{nameof(Account)}|{nameof(Title)}";
 
-    public static class Login
-    {
-        public const string LoginButtonLabel = $"{nameof(Login)}|{nameof(LoginButtonLabel)}";
-    }
-
-    public static class Pagination
-    {
-        public const string PreviousButtonLabel = $"{nameof(Pagination)}|{nameof(PreviousButtonLabel)}";
-        public const string NextButtonLabel = $"{nameof(Pagination)}|{nameof(NextButtonLabel)}";
+        public const string FirstNameLabel = $"{nameof(Account)}|{nameof(FirstNameLabel)}";
+        public const string LastNameLabel = $"{nameof(Account)}|{nameof(LastNameLabel)}";
+        public const string EmailNameLabel = $"{nameof(Account)}|{nameof(EmailNameLabel)}";
+        public const string RolesNameLabel = $"{nameof(Account)}|{nameof(RolesNameLabel)}";
     }
 
     public static class Catalog
     {
         public const string Title = $"{nameof(Catalog)}|{nameof(Title)}";
+
         public const string AddToBasketButtonLabel = $"{nameof(Catalog)}|{nameof(AddToBasketButtonLabel)}";
+    }
+
+    public static class Login
+    {
+        public const string Title = $"{nameof(Login)}|{nameof(Title)}";
+
+        public const string Header = $"{nameof(Login)}|{nameof(Header)}";
+        public const string EmailLabel = $"{nameof(Login)}|{nameof(EmailLabel)}";
+        public const string PasswordLabel = $"{nameof(Login)}|{nameof(PasswordLabel)}";
+        public const string LoginButtonLabel = $"{nameof(Login)}|{nameof(LoginButtonLabel)}";
+        public const string LogoutButtonLabel = $"{nameof(Login)}|{nameof(LogoutButtonLabel)}";
+    }
+
+    public static class Navigation
+    {
+        public const string OrdersLabel = $"{nameof(Navigation)}|{nameof(OrdersLabel)}";
+        public const string PaymentsLabel = $"{nameof(Navigation)}|{nameof(PaymentsLabel)}";
     }
 
     public static class Orders
@@ -35,6 +46,12 @@ public static class Resources
         public const string StatusColumnHeader = $"{nameof(Orders)}|{nameof(StatusColumnHeader)}";
     }
 
+    public static class Pagination
+    {
+        public const string PreviousButtonLabel = $"{nameof(Pagination)}|{nameof(PreviousButtonLabel)}";
+        public const string NextButtonLabel = $"{nameof(Pagination)}|{nameof(NextButtonLabel)}";
+    }
+
     public static class Payments
     {
         public const string Title = $"{nameof(Payments)}|{nameof(Title)}";
@@ -43,5 +60,15 @@ public static class Resources
         public const string PriceColumnHeader = $"{nameof(Payments)}|{nameof(PriceColumnHeader)}";
         public const string DateColumnHeader = $"{nameof(Payments)}|{nameof(DateColumnHeader)}";
         public const string StatusColumnHeader = $"{nameof(Payments)}|{nameof(StatusColumnHeader)}";
+    }
+
+    public static class Register
+    {
+        public const string Title = $"{nameof(Register)}|{nameof(Title)}";
+
+        public const string Header = $"{nameof(Register)}|{nameof(Header)}";
+        public const string EmailLabel = $"{nameof(Register)}|{nameof(EmailLabel)}";
+        public const string PasswordLabel = $"{nameof(Register)}|{nameof(PasswordLabel)}";
+        public const string RegisterButtonLabel = $"{nameof(Register)}|{nameof(RegisterButtonLabel)}";
     }
 }
