97f0e4d6a9855024451b3cdb9c5a5ea81513975a
Spec <dawidjachowicz@proton.me>
2023-08-11 17:18:51 +0200

    feature(client): Add Register view controller

---
+++ src/Clients/Web/Controllers/RegisterController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hermes.Client.Web.Controllers;
+
+public class RegisterController : Controller
+{
+    public IActionResult Index() =>
+        View();
+}
