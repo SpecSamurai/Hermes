9e9b37225d4155fc77fc3360f9434df34d267f01
Spec <dawidjachowicz@proton.me>
2023-08-22 20:56:07 +0200

    feature(client): Add ItemType view models

---
+++ src/Clients/Web/Models/Admin/ItemTypeViewModel.cs
@@ -0,0 +1,7 @@
+namespace Hermes.Client.Web.Models.Admin;
+
+internal class ItemTypeViewModel
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+}
---
+++ src/Clients/Web/Models/Admin/ItemTypesViewModel.cs
@@ -0,0 +1,6 @@
+namespace Hermes.Client.Web.Models.Admin;
+
+internal class ItemTypesViewModel
+{
+    public required IEnumerable<ItemTypeViewModel> ItemTypes { get; init; }
+}
