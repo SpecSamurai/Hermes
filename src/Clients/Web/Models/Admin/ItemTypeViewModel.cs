a1ba85519b42b8e1c9ce02dbddc4c0ae9c1c7c86
Spec <dawidjachowicz@proton.me>
2023-08-26 10:27:50 +0200

    feature(client): Delete obsolete classes

--- src/Clients/Web/Models/Admin/ItemTypeViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace Hermes.Client.Web.Models.Admin;
-
-internal class ItemTypeViewModel
-{
-    public required Guid Id { get; init; }
-    public required string Name { get; init; }
-}
--- src/Clients/Web/Models/Admin/ItemTypesViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace Hermes.Client.Web.Models.Admin;
-
-internal class ItemTypesViewModel
-{
-    public required IEnumerable<ItemTypeViewModel> ItemTypes { get; init; }
-}
