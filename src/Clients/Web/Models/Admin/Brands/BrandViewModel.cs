318dc9dff79f2eb5e8101a9156af696d97172482
Spec <dawidjachowicz@proton.me>
2023-08-26 10:21:35 +0200

    feature(client): Add Brands view models

---
+++ src/Clients/Web/Models/Admin/Brands/BrandViewModel.cs
@@ -0,0 +1,7 @@
+namespace Hermes.Client.Web.Models.Admin.Brands;
+
+class BrandViewModel
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+}
---
+++ src/Clients/Web/Models/Admin/Brands/BrandsViewModel.cs
@@ -0,0 +1,6 @@
+namespace Hermes.Client.Web.Models.Admin.Brands;
+
+class BrandsViewModel
+{
+    public required IEnumerable<BrandViewModel> Brands { get; init; }
+}
