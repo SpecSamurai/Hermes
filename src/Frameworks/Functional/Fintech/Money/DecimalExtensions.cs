73ce104287b4cd79647a984064854138518a7833
Spec <dawidjachowicz@proton.me>
2023-09-01 15:57:28 +0200

    refactor(fintech): Refactor DecimalExtensions

--- src/Frameworks/Functional/Fintech/Money/DecimalExtensions.cs
+++ src/Frameworks/Functional/Fintech/Money/DecimalExtensions.cs
@@ -3,5 +3,5 @@ namespace Fintech;
 public static class DecimalExtensions
 {
     public static Money ToMoney(this decimal amount, Currency currency) =>
-        new Money(amount, currency);
+        new(amount, currency);
 }
