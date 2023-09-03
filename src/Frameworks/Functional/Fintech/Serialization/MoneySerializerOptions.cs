11bf80872bbd4f83658e7f42460e9d31379019ff
Spec <dawidjachowicz@proton.me>
2023-09-03 17:01:08 +0200

    feature(fintech): Add Decimal format provider to MoneySerializerOptions

--- src/Frameworks/Functional/Fintech/Serialization/MoneySerializerOptions.cs
+++ src/Frameworks/Functional/Fintech/Serialization/MoneySerializerOptions.cs
@@ -17,6 +17,9 @@ public sealed class MoneySerializerOptions
     public static MoneySerializerOptions Default { get; }
 
     public CurrencyNamingPolicy CurrencyNamingPolicy { internal get; init; }
-    public required IReadOnlyDictionary<string, Currency> Currencies { internal get; init; }
-    public required IEnumerable<IMoneyConverter> Converters { internal get; init; }
+    public string? DecimalFormat { internal get; init; }
+    public IFormatProvider? DecimalFormatProvider { internal get; init; }
+
+    public required IReadOnlyDictionary<string, Currency> Currencies { get; init; }
+    public required IEnumerable<IMoneyConverter> Converters { get; init; }
 }
