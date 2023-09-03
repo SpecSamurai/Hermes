e923fdd54bda4df69a6b8d918723688fdaf1540e
Spec <dawidjachowicz@proton.me>
2023-09-03 17:02:14 +0200

    feature(fintech): Add Decimal formatting to StringToMoneyConverter

--- src/Frameworks/Functional/Fintech/Serialization/StringToMoneyConverter.cs
+++ src/Frameworks/Functional/Fintech/Serialization/StringToMoneyConverter.cs
@@ -7,16 +7,12 @@ public class StringToMoneyConverter : IMoneyConverter<string>
 
     public string Read(Money value, MoneySerializerOptions moneySerializerOptions)
     {
-        var currency = moneySerializerOptions.CurrencyNamingPolicy switch
-        {
-            CurrencyNamingPolicy.AlphabeticCode => value.Currency.AlphabeticCode,
-            CurrencyNamingPolicy.EnglishName => value.Currency.EnglishName,
-            CurrencyNamingPolicy.Symbol => value.Currency.Symbol,
-            _ => throw new ArgumentException(
-                "Invalid enum value for currency naming policy.", nameof(moneySerializerOptions.CurrencyNamingPolicy)),
-        };
+        var currency = value.Currency.ToString(moneySerializerOptions.CurrencyNamingPolicy);
+        var amount = value.Amount.ToString(
+            moneySerializerOptions.DecimalFormat,
+            moneySerializerOptions.DecimalFormatProvider);
 
-        return $"{value.Amount} {currency}";
+        return $"{amount} {currency}";
     }
 
     public Money? Write(string value, MoneySerializerOptions moneySerializerOptions)
