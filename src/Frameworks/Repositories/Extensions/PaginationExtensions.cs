e37938360786ffd3cf7a6af38e663210cfd9f98f
Spec <dawidjachowicz@proton.me>
2023-09-11 13:44:15 +0200

    feature(repositories): Fix ToPage implementation and unit test

--- src/Frameworks/Repositories/Extensions/PaginationExtensions.cs
+++ src/Frameworks/Repositories/Extensions/PaginationExtensions.cs
@@ -4,15 +4,9 @@ namespace System.Collections.Generic;
 
 public static class PaginationExtensions
 {
-    public static Page<T> ToPage<T>(this IEnumerable<T> collection, int totalCount, int pageIndex, int pageSize)
-    {
-        var pageCollection = collection.Skip(pageIndex * pageSize).Take(pageSize).ToList();
-        return new Page<T>(pageCollection, totalCount, pageIndex, pageSize);
-    }
-
     public static Page<T> ToPage<T>(this IEnumerable<T> collection, int totalCount, Offset offset)
     {
         var pageIndex = offset.Skip / offset.Take;
-        return collection.ToPage(totalCount, pageIndex, offset.Take);
+        return new Page<T>(collection.ToList(), totalCount, pageIndex, offset.Take);
     }
 }
--- tests/Frameworks/RepositoriesTest/PaginationExtensionsTest.cs
+++ tests/Frameworks/RepositoriesTest/PaginationExtensionsTest.cs
@@ -5,31 +5,10 @@ namespace Hermes.Frameworks.RepositoriesTest;
 public class PaginationExtensionsTest
 {
     [Fact]
-    public void ToPage_ValidInput_ValidPage()
-    {
-        // Arrange
-        var expectedList = new List<int> { 4, 5, 6 };
-        var expectedPageIndex = 1;
-        var expectedPageSize = 3;
-
-        var sut = Enumerable.Range(1, 10);
-
-        // Act
-        var actual = sut.ToPage(sut.Count(), expectedPageIndex, expectedPageSize);
-
-        // Assert
-        Assert.Equal(expectedPageIndex, actual.PageIndex);
-        Assert.Equal(expectedPageSize, actual.PageSize);
-        Assert.Equal(expectedList, actual);
-    }
-
-    [Fact]
     public void ToPage_ValidOffset_ValidPage()
     {
         // Arrange
-        var expectedList = new List<int> { 4, 5, 6 };
         var offset = Offset.Of(1, 3);
-
         var sut = Enumerable.Range(1, 10);
 
         // Act
@@ -38,6 +17,6 @@ public class PaginationExtensionsTest
         // Assert
         Assert.Equal(1, actual.PageIndex);
         Assert.Equal(3, actual.PageSize);
-        Assert.Equal(expectedList, actual);
+        Assert.Equal(sut, actual);
     }
 }
