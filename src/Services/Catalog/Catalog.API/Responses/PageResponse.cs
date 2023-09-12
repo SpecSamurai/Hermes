9933186d7dbc1db8d73748717f7b24e169db245b
Spec <dawidjachowicz@proton.me>
2023-09-12 16:27:35 +0200

    feature(catalog): Add TotalCount and TotalPages to PageResponse model

--- src/Services/Catalog/Catalog.API/Responses/PageResponse.cs
+++ src/Services/Catalog/Catalog.API/Responses/PageResponse.cs
@@ -3,6 +3,8 @@ namespace Hermes.Catalog.API.Responses;
 public record PageResponse<TModel>
 {
     public required IEnumerable<TModel> Data { get; init; }
+    public required int TotalCount { get; init; }
     public required int PageIndex { get; init; }
+    public required int TotalPages { get; init; }
     public required int PageSize { get; init; }
 }
