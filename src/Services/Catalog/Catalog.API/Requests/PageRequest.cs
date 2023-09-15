04700b472dd962530e592d87f675f39de5a19c8f
Spec <dawidjachowicz@proton.me>
2023-09-15 14:36:46 +0200

    feature(catalog): Adjust PageRequest validation

--- src/Services/Catalog/Catalog.API/Requests/PageRequest.cs
+++ src/Services/Catalog/Catalog.API/Requests/PageRequest.cs
@@ -7,7 +7,7 @@ public class PageRequest
     [Range(0, int.MaxValue, ErrorMessage = $"{nameof(Index)} can't be negative.")]
     public int Index { get; set; }
 
-    [Range(0, int.MaxValue, ErrorMessage = $"{nameof(Size)} can't be negative.")]
+    [Range(1, int.MaxValue, ErrorMessage = $"{nameof(Size)} can't be smaller than 1.")]
     public int Size { get; set; }
 
     public string? Sorting { get; set; }
